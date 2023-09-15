a8b88e92c37bc718c3310c052629b7093d2b0ff7
SamuelZelenka <samuel@actionsquadstudios.com>
2023-09-15 11:41:08 +0200

    Added Gamesession

---
+++ Assets/GameSession.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+
+using UnityEngine;
+
+public class GameSession : MonoBehaviour
+{
+	public bool isRacing;
+	public bool isRaceDone;
+	public float Timer => _timer;
+	private float _timer;
+
+	public static CarController Player => _instance._player;
+	[SerializeField]
+	private CarController _player;
+
+	public static Path Path => _instance._path;
+	[SerializeField]
+	private Path _path;
+
+	public int CurrentLap => _currentLap;
+	[ReadOnly(true)]
+	[SerializeField]
+	private int _currentLap;
+
+	public int TotalLaps => _totalLaps;
+
+	[SerializeField]
+	private int _totalLaps;
+	public static GameSession Instance => _instance;
+	private static GameSession _instance;
+
+	private void Awake()
+	{
+		if (_instance != null && _instance != this)
+		{
+			Destroy(this.gameObject);
+			return;
+		}
+
+		_instance = this;
+
+		DontDestroyOnLoad(this.gameObject);
+	}
+
+	// Your Start and Update methods go here
+	void Start()
+	{
+		isRacing = false;
+		isRaceDone = false;
+	}
+
+	void Update()
+	{
+		if (isRacing && !isRaceDone)
+		{
+			_timer += Time.deltaTime;
+		}
+		if (_currentLap == _totalLaps)
+		{
+			isRacing = false;
+			isRaceDone = true;
+		}
+	}
+
+	public void LapDone()
+	{
+		_currentLap++;
+	}
+}
