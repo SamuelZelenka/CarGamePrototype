e984f2f01d094fee47a847065372c6c5a3c271d3
SamuelZelenka <samuel@actionsquadstudios.com>
2023-09-15 11:42:02 +0200

    Added Timer UI

---
+++ Assets/TimerUI.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TimerUI : MonoBehaviour
+{
+    [SerializeField]
+    private TMP_Text _text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		float totalTime = GameSession.Instance.Timer;
+		int minutes = Mathf.FloorToInt(totalTime / 60);
+		int seconds = Mathf.FloorToInt(totalTime % 60);
+		int milliseconds = Mathf.FloorToInt((totalTime * 1000) % 1000); 
+
+		string formattedTime = string.Format("{0:D2}:{1:D2}:{2:D3}", minutes, seconds, milliseconds);
+
+        _text.text = formattedTime;
+    }
+}
