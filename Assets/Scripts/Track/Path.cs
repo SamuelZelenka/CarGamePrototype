ed79fe10b8b41f8a9828565e89d2075986a6c5f6
SamuelZelenka <samuel@actionsquadstudios.com>
2023-09-15 11:48:25 +0200

    Formated path calculation

--- Assets/Scripts/Track/Path.cs
+++ Assets/Scripts/Track/Path.cs
@@ -28,9 +28,9 @@ public class Path : MonoBehaviour
 
 		Vector3 interpolatedPoint =
 			0.5f * ((2 * p1) +
-					(-p0 + p2) * t +
-					(2 * p0 - 5 * p1 + 4 * p2 - p3) * t2 +
-					(-p0 + 3 * p1 - 3 * p2 + p3) * t3);
+			(-p0 + p2) * t +
+			(2 * p0 - 5 * p1 + 4 * p2 - p3) * t2 +
+			(-p0 + 3 * p1 - 3 * p2 + p3) * t3);
 
 		return interpolatedPoint;
 	}
