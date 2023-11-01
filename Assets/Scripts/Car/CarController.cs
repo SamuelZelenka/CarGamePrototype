04498ee8e377046fef7fe14caff917e721b07e14
SamuelZelenka <samuel@actionsquadstudios.com>
2023-11-01 03:41:44 +0100

    Updated Car controller to use Init instead of Start

--- Assets/Scripts/Car/CarController.cs
+++ Assets/Scripts/Car/CarController.cs
@@ -1,3 +1,5 @@
+using Sirenix.Utilities;
+using System.IO;
 using Unity.Collections;
 using UnityEngine;
 
@@ -10,11 +12,14 @@ public class CarController : MonoBehaviour
 
 	[Range(0, 100)]
 	[SerializeField]
-	private float idleBrakeAmount = 10;
+	private float _idleBrakeAmount = 10;
 
 	[Range(0, 10)]
 	[SerializeField]
-	private float slidingBrakeAmount = 10;
+	private float _slidingBrakeAmount = 10;
+
+	[SerializeField]
+	private float _maxVelocity;
 
 	[Range(0, 100)]
 	[SerializeField]
@@ -39,6 +44,9 @@ public class CarController : MonoBehaviour
 	private float _currentSlideCorrection = 0;
 
 	[SerializeField]
+	private float _slidingForceThreshold;
+
+	[SerializeField]
     private Rigidbody _rb;
 
 	[SerializeField]
@@ -48,32 +56,34 @@ public class CarController : MonoBehaviour
 	private Vector3 _target;
 
 	[SerializeField]
-	private Transform _targetVisual;
-
-	[SerializeField]
-	float _turnSpeed;
-	[SerializeField]
 	Transform _rightTire;
 
 	[SerializeField]
 	Transform _leftTire;
 
+	[SerializeField]
+	TireTackGenerator _tireTrackGenerator;
+
 	private Quaternion _rotationToTarget;
 
 	private float _targetPathTValue;
+
+	public float TargetPathTValue => _targetPathTValue;
+
 
 	private Vector3 _previousVelocity;
 	private Vector3 _velocity;
 
 	private bool _isAccelerating;
 
-    void Start()
+	private void Awake()
+	{
+		GameManager.PlayerManager.player = this;
+	}
+
+	public void Init()
     {
-		_targetPathTValue = 0;
-		_rb = GetComponent<Rigidbody>();
-		var path = GameSession.Path;
-		_target = path.GetPos(_targetPathTValue);
-		transform.position = _target;
+
 	}
 
 	void Update()
@@ -96,41 +106,64 @@ public class CarController : MonoBehaviour
 		CheckFullStopEstimation();
 
 		ApplyVelocity();
-		UpdateTargetPos();
 
 		_rb.velocity = _velocity;
 		_previousVelocity = _velocity;
 	}
 
+	public float GetVelocityPercentage()
+	{
+		return _velocity.magnitude / _maxVelocity;
+	}
+
+	public float GetAccelerationPercentage()
+	{
+		return _currentAcceleration / _maxAcceleration;
+	}
 	public Vector3 GetForce() => transform.position + _rb.velocity / 10;
 	public Vector3 GetTargetDir() => transform.position + (_target - transform.position);
 	public Vector3 GetAcceleration() => transform.position + transform.forward * (_currentAcceleration / _maxAcceleration) * 5;
-	public Vector3 GetSlidingCorrection() => transform.position + transform.right * _currentSlideCorrection / slidingBrakeAmount;
+	public Vector3 GetSlidingCorrection() => transform.position + transform.right * _currentSlideCorrection / _slidingBrakeAmount;
+
+	public bool IsSliding() => (transform.right * _currentSlideCorrection / _slidingBrakeAmount).sqrMagnitude > _slidingForceThreshold;
 
 	public void AccelerateInput(bool isAccelerating)
 	{
 		_isAccelerating = isAccelerating;
 	}
-
+	public void ResetCar()
+	{
+		_tireTrackGenerator.Clear();
+		_targetPathTValue = 0;
+		_rb = _rb != null ? _rb : GetComponent<Rigidbody>();
+		_rb.velocity = Vector3.zero;
+		_rb.Sleep();
+		_velocity = Vector3.zero;
+		_previousVelocity = Vector3.zero;
+		_currentSlideCorrection = 0;
+		_currentAcceleration = 0;
+		_target = Path.GetPos(_targetPathTValue, GameManager.TrackManager.ControlPoints);
+		transform.position = _target;
+	}
 	private void MoveTarget()
 	{
 		var isTargetTooClose = Vector3.Distance(transform.position, _target) < distToTarget;
-		var path = GameSession.Path;
 		if (isTargetTooClose)
 		{
-			var nextPos = path.GetPos(_targetPathTValue);
+			var controlPoints = GameManager.TrackManager.ControlPoints;
+			var nextPos = Path.GetPos(_targetPathTValue, controlPoints);
 			while (isTargetTooClose)
 			{
 				_targetPathTValue += 0.01f;
 				_targetPathTValue = ClampTToPath(_targetPathTValue);
-				nextPos = path.GetPos(_targetPathTValue);
+				nextPos = Path.GetPos(_targetPathTValue, controlPoints);
 				isTargetTooClose = Vector3.Distance(transform.position, nextPos) < distToTarget;
 			}
 			_target = nextPos;
 		}
 	}
 
-	private float ClampTToPath(float t) => t % GameSession.Path.controlPoints.Length;
+	private float ClampTToPath(float t) => t % GameManager.TrackManager.ControlPoints.Count;
 
 	private void MoveChaser()
 	{
@@ -148,29 +181,34 @@ public class CarController : MonoBehaviour
 
 	private void ApplySlidingFriction(float slidingDirection)
 	{
-		_velocity += transform.right * slidingDirection * slidingBrakeAmount;
+		_velocity += transform.right * slidingDirection * _slidingBrakeAmount;
 	}
 
 	private void ApplyTireRotation(float slidingDirection)
 	{
 		const float MAX_ANGLE = 25;
-		var angle = Mathf.Clamp(MAX_ANGLE * -slidingDirection, -MAX_ANGLE, MAX_ANGLE);
+		var angle = Mathf.Clamp(MAX_ANGLE * -slidingDirection * 0.2f, -MAX_ANGLE, MAX_ANGLE);
 
 		if (!_isAccelerating || Mathf.Abs(slidingDirection) < 0.2f)
 		{
 			angle = 0;
 		}
+		var oldRot = _rightTire.localRotation;
 		var newRot = Quaternion.Euler(0, 0, angle);
 
-		_rightTire.localRotation = newRot;
-		_leftTire.localRotation = newRot;
+		_rightTire.localRotation = Quaternion.Lerp(oldRot, newRot, Time.deltaTime * turnSpeed) ;
+		_leftTire.localRotation = Quaternion.Lerp(oldRot, newRot, Time.deltaTime * turnSpeed);
 	}
 
 	private void ApplyAcceleration(float forwardDir)
 	{
-		if (_isAccelerating)
+		if (_isAccelerating && !IsSliding())
 		{
 			Accelerate();
+			//if (!IsOnRoad())
+			//{
+			//	_currentAcceleration = _minAcceleration;
+			//}
 		}
 		else if (_currentAcceleration > 0)
 		{
@@ -178,8 +216,10 @@ public class CarController : MonoBehaviour
 		}
 		else if (forwardDir > 0.01f)
 		{
-			_velocity -= transform.forward * forwardDir * idleBrakeAmount;
+			_velocity -= transform.forward * forwardDir * _idleBrakeAmount;
 		}
+
+
 
 		_velocity += transform.forward * _currentAcceleration;
 	}
@@ -196,6 +236,29 @@ public class CarController : MonoBehaviour
 	private void Decelerate(float forwardDir)
 	{
 		_currentAcceleration = Mathf.Max(0, _currentAcceleration - (_deceleration * Time.fixedDeltaTime));
+	}
+
+	private RaycastHit hit;
+
+	private bool IsOnRoad()
+	{
+		bool isOnRoad = true;
+
+		Debug.DrawRay(_leftTire.transform.position, _leftTire.transform.forward);
+		Physics.Raycast(_leftTire.transform.position, -_leftTire.transform.up, out hit);
+		if (!hit.transform)
+			isOnRoad = false;
+
+
+		Debug.DrawRay(_rightTire.transform.position, _rightTire.transform.forward);
+
+		Physics.Raycast(_rightTire.transform.position, -_rightTire.transform.up, out hit);
+		if (!hit.transform)
+			isOnRoad = false;
+
+		Debug.Log(hit.transform);
+
+		return isOnRoad;
 	}
 
 	private void CheckMaxAcceleration()
@@ -216,14 +279,9 @@ public class CarController : MonoBehaviour
 
 	private void ApplyVelocity()
 	{
+		_velocity = Vector3.ClampMagnitude(_velocity, _maxVelocity);
 		_velocity = Vector3.Lerp(_previousVelocity, _velocity, Time.fixedDeltaTime);
 		_velocity.y = 0;
 		_rb.MovePosition(new Vector3(transform.position.x, _target.y + 0.5f,transform.position.z));
-	}
-
-	private void UpdateTargetPos()
-	{
-		var nextPos = Vector3.Slerp(_targetVisual.transform.position, _target, 25 * Time.fixedDeltaTime);
-		_targetVisual.transform.position = nextPos;
 	}
 }
